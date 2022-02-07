553a5e54aae9cd2cd08525354c6ecbf2c20faad1
ShiFa-is <joint.shpn@gmail.com>
2022-02-07 13:53:22 +0000

    バリデーション

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_17_130014) do
+ActiveRecord::Schema.define(version: 2022_02_07_133619) do
 
   create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
   end
 
 end
